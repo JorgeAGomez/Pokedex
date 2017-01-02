a23e44e257967a3aa5b0a10d5465d72d744e9b28
Jorge <jorge_gomez12@hotmail.com>
2017-01-01 20:41:26 -0700

    Music can be played now.

--- Pokedex/ViewController.swift
+++ Pokedex/ViewController.swift
@@ -7,12 +7,16 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
 
 
   @IBOutlet weak var collectionView: UICollectionView!
   var pokemons = [Pokemon]()
+  
+  var musicPlayer: AVAudioPlayer!
+  
   
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -21,6 +25,21 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
     collectionView.dataSource = self
     
     parsePokemonCSV()
+    initAudio()
+  }
+  
+  func initAudio(){
+    let path = Bundle.main.path(forResource: "music", ofType: "mp3")!
+    
+    do {
+      musicPlayer = try AVAudioPlayer(contentsOf: URL(string: path)!)
+      musicPlayer.prepareToPlay()
+      musicPlayer.numberOfLoops = -1
+      musicPlayer.play()
+      
+    } catch let error as NSError {
+      print(error.debugDescription)
+    }
   }
   
   func parsePokemonCSV(){
@@ -66,7 +85,7 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
 
   func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
   
-    return 30
+    return pokemons.count
   }
   
   func numberOfSections(in collectionView: UICollectionView) -> Int {
@@ -78,5 +97,21 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
   
     return CGSize(width: 105, height: 105)
   }
+  
+  
+  @IBAction func musicButtonPressed(_ sender: UIButton) {
+  
+    if musicPlayer.isPlaying {
+    
+      musicPlayer.pause()
+      sender.alpha = 0.2
+    }
+    else {
+    
+      musicPlayer.play()
+      sender.alpha = 0.1
+    }
+  }
+  
 }
 
